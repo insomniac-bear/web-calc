7aef36c1a723cb2495b88126083b1f15a94d8e6a
K-Tcherapanov <tcherepanov.kir@gmail.com>
2021-08-14 14:09:35 +0300

    fixed bug with check role on server

--- server/middleware/role-check-middleware.js
+++ server/middleware/role-check-middleware.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 module.exports.roleCheckMiddleware = (req, res, next) => {
   try {
     const { role } = req.user;
-    if (role !== 'admin') {
+    if (role !== 'admin' || role !== 'moderator') {
       return res
         .status(StatusCodes.FORBIDDEN)
         .json({ message: 'Not enough authority' });
